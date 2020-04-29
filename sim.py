217409699553dbb6f23e69692ed05e29f1e34d63
Florent Kermarrec <florent@enjoy-digital.fr>
2020-04-29 20:13:39 +0200

    add initial sim with VexRiscv SMP.

---
+++ sim.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from migen import *
+
+from litex.build.generic_platform import *
+from litex.build.sim import SimPlatform
+from litex.build.sim.config import SimConfig
+
+from litex.soc.interconnect.csr import *
+from litex.soc.integration.soc import SoCRegion
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.soc_sdram import *
+from litex.soc.integration.builder import *
+from litex.soc.interconnect import wishbone
+
+from litedram import modules as litedram_modules
+from litedram.phy.model import SDRAMPHYModel
+from litex.tools.litex_sim import sdram_module_nphases, get_sdram_phy_settings
+
+from vexriscv_smp import VexRiscvSMP
+
+# IOs ----------------------------------------------------------------------------------------------
+
+_io = [
+    ("sys_clk", 0, Pins(1)),
+    ("sys_rst", 0, Pins(1)),
+    ("serial", 0,
+        Subsignal("source_valid", Pins(1)),
+        Subsignal("source_ready", Pins(1)),
+        Subsignal("source_data",  Pins(8)),
+
+        Subsignal("sink_valid", Pins(1)),
+        Subsignal("sink_ready", Pins(1)),
+        Subsignal("sink_data",  Pins(8)),
+    ),
+]
+
+# Platform -----------------------------------------------------------------------------------------
+
+class Platform(SimPlatform):
+    def __init__(self):
+        SimPlatform.__init__(self, "SIM", _io)
+
+# Supervisor ---------------------------------------------------------------------------------------
+
+class Supervisor(Module, AutoCSR):
+    def __init__(self):
+        self._finish  = CSR()  # controlled from CPU
+        self.finish = Signal() # controlled from logic
+        self.sync += If(self._finish.re | self.finish, Finish())
+
+# SoCSMP -------------------------------------------------------------------------------------------
+
+class SoCSMP(SoCSDRAM):
+    csr_map = {**SoCSDRAM.csr_map, **{
+        "ctrl":       0,
+        "uart":       2,
+        "timer0":     3,
+    }}
+    interrupt_map = {**SoCSDRAM.interrupt_map, **{
+        "uart":       0,
+        "timer0":     1,
+    }}
+    mem_map = {**SoCSDRAM.mem_map, **{
+        "csr":          0xf0000000,
+    }}
+
+    def __init__(self,
+        init_memories         = False,
+        with_sdram            = True,
+        sdram_module          = "MT41K128M16",
+        sdram_data_width      = 16,
+        sdram_verbosity       = 0,
+        with_ethernet         = False):
+        platform     = Platform()
+        sys_clk_freq = int(1e6)
+
+        ram_init = []
+        if init_memories:
+            ram_init = [] # FIXME
+
+        # SoCSDRAM ----------------------------------------------------------------------------------
+        SoCSDRAM.__init__(self, platform, clk_freq=sys_clk_freq,
+            cpu_type                 = "vexriscv", cpu_variant="standard", cpu_cls=VexRiscvSMP,
+            uart_name                = "sim",
+            l2_reverse               = False,
+            max_sdram_size           = 0x10000000, # Limit mapped SDRAM to 1GB.
+            integrated_rom_size      = 0x8000,
+            integrated_main_ram_size = 0x00000000 if with_sdram else 0x02000000, # 32MB
+            integrated_main_ram_init = [] if (with_sdram or not init_memories) else ram_init)
+        self.add_constant("SIM")
+
+        # CLINT ------------------------------------------------------------------------------------
+        self.bus.add_slave("clint", self.cpu.cbus, region=SoCRegion(size=0x10000, cached=False))
+
+        # Supervisor -------------------------------------------------------------------------------
+        self.submodules.supervisor = Supervisor()
+        self.add_csr("supervisor")
+
+        # CRG --------------------------------------------------------------------------------------
+        self.submodules.crg = CRG(platform.request("sys_clk"))
+
+        # SDRAM ------------------------------------------------------------------------------------
+        if with_sdram:
+            sdram_clk_freq   = int(100e6) # FIXME: use 100MHz timings
+            sdram_module_cls = getattr(litedram_modules, sdram_module)
+            sdram_rate       = "1:{}".format(sdram_module_nphases[sdram_module_cls.memtype])
+            sdram_module     = sdram_module_cls(sdram_clk_freq, sdram_rate)
+            phy_settings     = get_sdram_phy_settings(
+                memtype    = sdram_module.memtype,
+                data_width = sdram_data_width,
+                clk_freq   = sdram_clk_freq)
+            self.submodules.sdrphy = SDRAMPHYModel(
+                module    = sdram_module,
+                settings  = phy_settings,
+                clk_freq  = sdram_clk_freq,
+                verbosity = sdram_verbosity,
+                init      = ram_init)
+            self.register_sdram(
+                self.sdrphy,
+                sdram_module.geom_settings,
+                sdram_module.timing_settings)
+            # FIXME: skip memtest to avoid corrupting memory
+            self.add_constant("MEMTEST_BUS_SIZE",  0)
+            self.add_constant("MEMTEST_ADDR_SIZE", 0)
+            self.add_constant("MEMTEST_DATA_SIZE", 0)
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    parser = argparse.ArgumentParser(description="Linux on LiteX-VexRiscv Simulation")
+    parser.add_argument("--sdram-verbosity",      default=0,               help="Set SDRAM checker verbosity")
+    parser.add_argument("--trace",                action="store_true",     help="enable VCD tracing")
+    parser.add_argument("--trace-start",          default=0,               help="cycle to start VCD tracing")
+    parser.add_argument("--trace-end",            default=-1,              help="cycle to end VCD tracing")
+    parser.add_argument("--opt-level",            default="O3",            help="compilation optimization level")
+    args = parser.parse_args()
+
+    sim_config = SimConfig(default_clk="sys_clk")
+    sim_config.add_module("serial2console", "serial")
+
+    for i in range(2):
+        soc = SoCSMP(i!=0, sdram_verbosity=int(args.sdram_verbosity))
+        board_name = "sim"
+        build_dir = os.path.join("build", board_name)
+        builder = Builder(soc, output_dir=build_dir,
+            compile_gateware = i!=0,
+            csr_json         = os.path.join(build_dir, "csr.json"))
+        builder.build(sim_config=sim_config,
+            run         = i!=0,
+            opt_level   = args.opt_level,
+            trace       = args.trace,
+            trace_start = int(args.trace_start),
+            trace_end   = int(args.trace_end))
+
+if __name__ == "__main__":
+    main()
